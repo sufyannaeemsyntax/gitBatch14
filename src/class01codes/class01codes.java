64d878a5b22466bed4b87cf78f2e1dfffc38980c
sufyannaeemsytnax <sufinani1@gmail.com>
2022-09-17 16:43:35 -0500

    first commit

---
+++ src/class01codes/class01codes.java
@@ -0,0 +1,11 @@
+package class01codes;
+
+public class class01codes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// code to launch the nuclear weapon
+		System.out.println("Launch my favorite Tv show");
+	}
+
+}
